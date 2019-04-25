1a0af169d088225a145ad82646a0bd8b76a05e6c
Jonathan <jonathan@styqr.fr>
2019-04-25 23:15:53 +0200

    Page tarifs

--- tarifs.php
+++ tarifs.php
@@ -6,39 +6,45 @@
             <div class="container">
                 <div class="line-fonctionnement">
             <ul>
-                <li><img src="img/creer.png" alt="creer-un-spliiit"/><p>Création du compte.</p></li>
-                <li><img src="img/inscription.png" alt="abonnement-spliiit"/><p>Création d'un spliiit.</p></li>
-                <li><img src="img/creditcard.png" alt="payer-par-cb"/><p>Participation des invités.</p></li>
+                <li><img src="img/creer.png" alt="creer-un-spliiit"/><p><strong>Création</strong><br>du compte<br><strong class="free-tarif">gratuit</strong></p></li>
+                <li><img src="img/inscription.png" alt="abonnement-spliiit"/><p><strong>Création</strong><br>d'un spliiit<br><strong class="free-tarif">gratuit</strong></p></li>
+                <li><img src="img/creditcard.png" alt="payer-par-cb"/><p><strong>Participation</strong><br>des invités<br><strong class="free-tarif">0,10€ + 5%</strong></p></li>
             </ul>
         </div>
             </div>
         </section>
         <section class="tarif-partner">
             <div class="bg-rotate grey-background"></div>
-            <div class="container">
-            <div class="width50">
+            <div class="container line-tarif">
+            <div class="width50 left-line-tarif">
                 <h2>Dépensez chez nos partenaires</h2>
-                <p></p>
+                <h3>Gratuit</h3>
+                <p>Transformez votre cagnotte en carte cadeau sur les sites de nos partenaires. Aucun frais n’est appliqué sur le montant dépensé.</p>
             </div>
-            <div class="width50"></div>
+            <div class="width50"><img class="from-right" src="img/coralie.png" alt="partage-licence-logiciel"/></div>
             </div>
         </section>
         <section class="tarif-dons">
-            <div class="container">
+            <div class="container line-tarif">
             <div class="width50">
+                <img class="from-left" src="img/coralie.png" alt="partage-licence-logiciel"/>
             </div>
-            <div class="width50">
+            <div class="width50 right-line-tarif">
                 <h2>Faire un don</h2>
+                <h3>Gratuit</h3>
+                <p>Transformez votre cagnotte en carte cadeau sur les sites de nos partenaires. Aucun frais n’est appliqué sur le montant dépensé.</p>
             </div>
             </div>
         </section>
         <section class="tarif-virement">
             <div class="bg-rotate grey-background"></div>
-            <div class="container">
-            <div class="width50">
+            <div class="container line-tarif">
+            <div class="width50 left-line-tarif">
                 <h2>Faire un virement</h2>
+                <h3>Gratuit</h3>
+                <p>Transformez votre cagnotte en carte cadeau sur les sites de nos partenaires. Aucun frais n’est appliqué sur le montant dépensé.</p>
             </div>
-            <div class="width50"></div>
+            <div class="width50"><img class="from-right" src="img/coralie.png" alt="partage-licence-logiciel"/></div>
             </div>
         </section>
 </section>
