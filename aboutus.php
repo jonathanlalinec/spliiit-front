91595290d4b4ffdc423e45d8151f8df7e837a05d
Jonathan <jonathan@styqr.fr>
2019-05-03 08:47:04 +0200

    Page Qui sommes-nous

--- aboutus.php
+++ aboutus.php
@@ -2,31 +2,42 @@
 <?php include 'header.php';?>
 <?php include 'header-page.php';?>
 
-<section class="whoare">
+<section class="team">
     <div class="container">
-        <img src="img/whoare.png"/>
-        <p><strong>Spliiit a été crée en 2019. C’est  l’idée de 3 potes : Stéphane, Brice et Jonathan.<br>
-</strong>Originaires de région Parisiennne, ils sont tous 3 complémentaires.</p>
-        <div class="bloc-white padding3050 margin50 flex">
-            <div class="width50"><img src="img/ste.png"/></div>
-            <div class="width50">
-            <h2>Stéphane</h2>
-            <p>Stéphane, dit « El Comunicator », polyglotte, connecté, il gère la communication et le réfèrencement de Spliiit à l’internationnal. Qui que tu sois « El Comunicator » sait qui tu es… <strong>Mouah Ah ah !</strong></p>
+    <img src="img/team.png"/>
+        <p><strong>Spliiit a été crée en 2019. C’est  l’idée de 3 potes : Brice, Jonathan et Stéphane.<br>
+</strong>Originaires de région Parisiennne, ils sont tous les 3 complémentaires.</p>
+    </div>
+</section>
+<section class="founder">
+    <div class="bg-rotate grey-background"></div>
+    <div class="container">
+        <h2>Les fondateurs</h2>
+        <div class="flex">
+        <div class="bloc-white founder-profil">
+            <img class="profil-founder" src="img/brice.jpg" alt="Brice Vincent"/>
+            <h3>Brice<br><strong>Vincent</strong></h3>
+            <p>Brice à commencé en tant que Webdesigner au sein de la première société de Jonathan en 2007, avec un Master de Communication visuelle et design numérique. Par la suite il rejoint comme directeur artistique la société Adverline (Rachetée par le groupe Laposte) où il s'occupera notamment des projets tels que Societe.com, Annuaire.com, Optelo… Il rejoint ensuite UMCgroup en tant que directeur artistique.</p>
+            <div class="link-linkedin">
+            <a href="https://www.linkedin.com/in/jonathanlalinec/" target="_blank"><img src="img/linkedin-team.png"/ alt="Logo LinkedIn"></a>
             </div>
         </div>
-        <div class="bloc-white padding3050 margin50 flex">
-            <div class="width50">
-                <h2>Brice</h2>
-                <p>Brice, dit « L’artiste », si vous nous lisez actuellement c’est que vous souhaitez en savoir plus sur nous ? Good :-) « L’artiste » à fait son job ! Il rend un service simple, beau et nous voulons tous en savoir plus !</p>
+        <div class="bloc-white founder-profil center-founder">
+            <img class="profil-founder" src="img/jonathan.jpg" alt="Jonathan Lalinec"/>
+                <h3>Jonathan<br><strong>Lalinec</strong></h3>
+                <p>Passionné par le domaine du digital depuis toujours, Jonathan a fondé sa première Start-Up à 17 ans avec seulement un BTS de Communication Visuelle en poche. A 21 ans, sa première société (plateforme éditrice de blogs) lève 900 000€. Elle sera revendu au groupe Webedia en 2016. Il co-fonde par la suite UMCgroup, une agence digitale éditrice notamment du CMS BeeHiv’ et plus récemment StyQR (Le livret d’accueil digital).</p>
+            <div class="link-linkedin">
+            <a href="https://www.linkedin.com/in/jonathanlalinec/" target="_blank"><img src="img/linkedin-team.png"/ alt="Logo LinkedIn"></a>
             </div>
-            <div class="width50"><img src="img/brice.png"/></div>
         </div>
-        <div class="bloc-white padding3050 margin50 flex">
-            <div class="width50"><img src="img/jo.png"/></div>
-            <div class="width50">
-            <h2>Jonathan</h2>
-            <p>Jonathan, je n'ai pas de mot pour moi même... :-D</p>
+        <div class="bloc-white founder-profil">
+            <img class="profil-founder" src="img/stephane.jpg" alt="Stephane Phung"/>
+            <h3>Stéphane<br><strong>Phung</strong></h3>
+            <p>Diplomé d’un BTS informatique de gestion, Stéphane a travaillé pour plusieurs SSII pendant une dizaine d'années et pour de grands groupes tels que GDF Suez ou BNP Paribas, en tant que Service Delivery Manager. Son intérêt pour le digital le pousse ensuite à se tourner vers la communication et les stratégies du web marketing. Il se forme ainsi au sein d'UMCgroup durant 2 ans sur la stratégie d'acquisition des réseaux sociaux.</p>
+            <div class="link-linkedin">
+            <a href="https://www.linkedin.com/in/jonathanlalinec/" target="_blank"><img src="img/linkedin-team.png"/ alt="Logo LinkedIn"></a>
             </div>
+        </div>
         </div>
     </div>
 </section>
